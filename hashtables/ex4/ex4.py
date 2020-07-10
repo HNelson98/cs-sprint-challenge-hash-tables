e981eeec602abc91e9e070ef578cc888fdb600ab
Henry Nelson <Henry.Nelson89@gmail.com>
2020-07-10 10:57:18 -0700

    tried making it faster

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,15 +3,23 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # storage = {}
+    # result = list()
+    # length = len(a)
+
+    # for i in range(length):
+    #     for j in range (i + 1, length):
+    #         if(abs(a[i]) == abs(a[j])):
+    #             result.append(abs(a[i]))
     storage = {}
     result = list()
     length = len(a)
 
     for i in range(length):
-        for j in range (i + 1, length):
-            if(abs(a[i]) == abs(a[j])):
-                result.append(abs(a[i]))
-
+        if abs(a[i]) not in storage:
+            storage[i] = abs(a[i])
+        if abs(a[i]) in storage:
+            result.append(abs(i))
     
     return result
 
